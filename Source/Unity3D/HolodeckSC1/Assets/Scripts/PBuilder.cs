dc13d7c2ed296e18f4b14577b2055f1f21efe2bf
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2015-04-26 22:01:04 +0200

    Szenario1 fertig.

---
+++ Source/Unity3D/HolodeckSC1/Assets/Scripts/PBuilder.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PBuilder : MonoBehaviour {
+	public float plattformSize;
+	public GameObject plattform;
+	public GameObject halt;
+	public GameObject quer;
+	private float haltPos = 0.65f;
+	private float querPos = 1.29f;
+	// Use this for initialization
+	void Start () {
+		GameObject newPlattform =  (GameObject) GameObject.Instantiate(plattform,transform.position,transform.rotation);
+		newPlattform.transform.localScale = new Vector3(plattformSize,0.1f,plattformSize);
+		newPlattform.SetActive(true);
+		//Haltebalcken
+		GameObject new1Halt =  (GameObject) GameObject.Instantiate(halt,new Vector3(plattformSize/2f,haltPos,plattformSize/2f),Quaternion.Euler(90f,0,0));
+		new1Halt.SetActive(true);
+		GameObject new2Halt =  (GameObject) GameObject.Instantiate(halt,new Vector3(plattformSize/-2f,haltPos,plattformSize/2f),Quaternion.Euler(90f,0,0));
+		new2Halt.SetActive(true);
+		GameObject new3Halt =  (GameObject) GameObject.Instantiate(halt,new Vector3(plattformSize/2f,haltPos,plattformSize/-2f),Quaternion.Euler(90f,0,0));
+		new3Halt.SetActive(true);
+		GameObject new4Halt =  (GameObject) GameObject.Instantiate(halt,new Vector3(plattformSize/-2f,haltPos,plattformSize/-2f),Quaternion.Euler(90f,0,0));
+		new4Halt.SetActive(true);
+		//Querbalcken unten
+		GameObject new1Quer =  (GameObject) GameObject.Instantiate(quer,new Vector3(plattformSize/2f,haltPos,0f),Quaternion.Euler(0f,0,0));
+		new1Quer.transform.localScale = new Vector3(0.02f,0.02f,plattformSize);
+		new1Quer.SetActive(true);
+		GameObject new2Quer =  (GameObject) GameObject.Instantiate(quer,new Vector3(plattformSize/-2f,haltPos,0f),Quaternion.Euler(0f,0,0));
+		new2Quer.transform.localScale = new Vector3(0.02f,0.02f,plattformSize);
+		new2Quer.SetActive(true);
+		GameObject new3Quer =  (GameObject) GameObject.Instantiate(quer,new Vector3(0f,haltPos,plattformSize/2f),Quaternion.Euler(0,90,0));
+		new3Quer.transform.localScale = new Vector3(0.02f,0.02f,plattformSize);
+		new3Quer.SetActive(true);
+		GameObject new4Quer =  (GameObject) GameObject.Instantiate(quer,new Vector3(0f,haltPos,plattformSize/-2f),Quaternion.Euler(0,90,0));
+		new4Quer.transform.localScale = new Vector3(0.02f,0.02f,plattformSize);
+		new4Quer.SetActive(true);
+		//Querbalcken oben
+		GameObject new5Quer =  (GameObject) GameObject.Instantiate(quer,new Vector3(plattformSize/2f,querPos,0f),Quaternion.Euler(0f,0,0));
+		new5Quer.transform.localScale = new Vector3(0.02f,0.02f,plattformSize);
+		new5Quer.SetActive(true);
+		GameObject new6Quer =  (GameObject) GameObject.Instantiate(quer,new Vector3(plattformSize/-2f,querPos,0f),Quaternion.Euler(0f,0,0));
+		new6Quer.transform.localScale = new Vector3(0.02f,0.02f,plattformSize);
+		new6Quer.SetActive(true);
+		GameObject new7Quer =  (GameObject) GameObject.Instantiate(quer,new Vector3(0f,querPos,plattformSize/2f),Quaternion.Euler(0,90,0));
+		new7Quer.transform.localScale = new Vector3(0.02f,0.02f,plattformSize);
+		new7Quer.SetActive(true);
+		GameObject new8Quer =  (GameObject) GameObject.Instantiate(quer,new Vector3(0f,querPos,plattformSize/-2f),Quaternion.Euler(0,90,0));
+		new8Quer.transform.localScale = new Vector3(0.02f,0.02f,plattformSize);
+		new8Quer.SetActive(true);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Source/Unity3D/HolodeckSC1/Assets/Scripts/TrackingController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using TrackingClassLibrary;
+using System.Collections.Generic;
+using System;
+using TrackingClassLibrary.Marker;
+
+[Serializable]
+public struct Entry
+{
+    public GameObject go;
+    public int trackID;
+
+}
+public class TrackingController : MonoBehaviour {
+
+    private Tracker tracker;
+    public string address = "230.230.230.230";
+    public int port = 5230;
+    public Dictionary<GameObject, int> registerObj = new Dictionary<GameObject,int>();
+    public Entry[] registerObjects;
+	void Start () {
+        tracker = new Tracker(address, port);
+        tracker.Start();
+        for (int i = 0; i < registerObjects.Length; i++)
+        {
+            Register(registerObjects[i].go, registerObjects[i].trackID);
+		}
+	}
+	
+	void Update () {
+        foreach (var item in registerObj)
+        {
+            IMarker m = tracker[item.Value];
+            item.Key.transform.position = new Vector3((float)(m.X/1000),(float)(m.Z/1000),(float)(m.Y/1000));
+            item.Key.transform.rotation = 
+                Quaternion.LookRotation(new Vector3((float)m.Rotationmatrix[1][0], (float)m.Rotationmatrix[1][2], (float)m.Rotationmatrix[1][1]),
+                new Vector3((float)m.Rotationmatrix[2][0], (float)m.Rotationmatrix[2][2], (float)m.Rotationmatrix[2][1]));
+        }
+	}
+
+    public void Register(GameObject gameObject, int trackID)
+    {
+        registerObj[gameObject] = trackID;
+    }
+
+    public void UnRegister(GameObject gameObject)
+    {
+        registerObj.Remove(gameObject);
+    }
+
+    public Vector3 PositionFor(int trackID)
+    {
+        IMarker m = tracker[trackID];
+        return new Vector3((float)(m.X / 1000), (float)(m.Z / 1000), (float)(m.Y / 1000));
+    }
+
+    public Quaternion RotationFor(int trackID)
+    {
+        IMarker m = tracker[trackID];
+        return Quaternion.LookRotation(new Vector3((float)m.Rotationmatrix[1][0], (float)m.Rotationmatrix[1][2], (float)m.Rotationmatrix[1][1]),
+                new Vector3((float)m.Rotationmatrix[2][0], (float)m.Rotationmatrix[2][2], (float)m.Rotationmatrix[2][1]));
+    }
+
+}
